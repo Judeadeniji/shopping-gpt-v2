02076ccbdc42f5218ce59104b7d01a80ccb70977
Okoro James <okorojameschizaram@gmail.com>
2023-05-04 07:45:17 +0100

    working on the navbar currently

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/nav/Header";
+
 function App() {
-  return <div className="app__container"></div>;
+  return (
+    <div className="app__container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">Shopping GPT</div>
+      <div className="navlinks__row">
+        {/* nav livks col 1 */}
+        <form className="navlinks__col1">
+          <input type="text" placeholder="search product" />
+          <button className="search__product__btn">
+            <i className="fa-solid fa-magnifying-glass search__product__icon"></i>
+          </button>
+        </form>
+        {/* nav livks col 2 */}
+        <div className="navlinks__col2">
+          <i className="fa-regular fa-bell"></i>
+          <i className="fa-regular fa-user"></i>
+          <i className="fa-solid fa-bag-shopping"></i>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
