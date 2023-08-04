2680801b33f830b0b8d2a71f1133d41db502969e
Okoro James <okorojameschizaram@gmail.com>
2023-08-04 20:14:15 +0100

    Update GetToTopOnRender.jsx

--- src/components/get-to-top/GetToTopOnRender.jsx
+++ src/components/get-to-top/GetToTopOnRender.jsx
@@ -1,7 +1,7 @@
 import {useEffect} from "react"
 import {useLocation} from "react-router-dom"
 
-export const GetToTopOnRender = () => {
+export default const GetToTopOnRender = () => {
   const pathname = useLocation()
   useEffect(()=>{
     window.scrollTo(0,0)
